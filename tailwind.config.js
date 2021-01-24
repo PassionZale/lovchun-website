f86b3616fd21e92d5f72b099f3c9ace4a7665c17
PassionZale <whouu@qq.com>
2021-01-24 12:46:06 +0800

    config: purge css 增加环境变量区分

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  purge: [
+  purge: process.env.NODE_ENV === 'producton' ? [
     './pages/**/*.{js,jsx,ts,tsx}',
     './components/**/*.{js,jsx,ts,tsx}',
     './styles/**/*.css'
-  ],
+  ] : [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
